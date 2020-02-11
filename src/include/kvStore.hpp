5d468ba07c220541c8167aaa20aa960a450bcea8
YoogottamK <yoogottamk@outlook.com>
2020-02-11 23:30:22 +0530

    basic class struct

---
+++ src/include/kvStore.hpp
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+struct Slice {
+    uint8_t size;
+    char * data;
+};
+
+class kvStore {
+    kvStore(uint64_t max_entries);
+    bool get(Slice & key, Slice & value);
+    bool put(Slice & key, Slice & value);
+    bool del(Slice & key);
+    bool get(int N, Slice& key, Slice& value);
+    bool del(int N);
+};
