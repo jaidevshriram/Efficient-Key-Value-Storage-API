1b6f1ec9ca22be17f6981a358946a2f76e8fed42
Jaidev Shriram <jaidevtrumpet@gmail.com>
2020-02-17 00:43:03 +0530

    Working on benchmark for trie

---
+++ evals/kvStore.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#include "../src/trie.cpp"
+#include "../src/include/kvStore.hpp"
+using namespace std;
+
+class kvstore{
+public:
+	Trie db;
+
+	bool get(string key){
+		Slice a(key), b;
+		bool is_valid = db.get_val(a, b);
+		return is_valid;
+	}
+
+	bool put(string key, string value){
+		Slice a(key), b(value);
+		db.insert(a, b));
+		return true;
+	}
+
+	bool del(string key){
+		Slice a(key);
+		bool is_valid = db.del(a);
+		return is_valid;
+	}
+
+	pair<string,string> get(int N){
+		// Your Code Here
+		pair<string,string> temp = make_pair("key","value");
+		return temp;
+	}
+
+	bool del(int N){
+		// Your Code Here
+		return true;
+	}
+};
