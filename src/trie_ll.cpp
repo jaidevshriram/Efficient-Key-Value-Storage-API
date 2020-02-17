64b1a033a3a89330423ae67ebc6ba549184eec3a
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-02-17 23:04:45 +0530

    added ll ins and del

--- src/trie_ll.cpp
+++ src/trie_ll.cpp
@@ -28,7 +28,7 @@ class Trie {
     };
 
     struct LLNode {
-        int letter;
+        char letter;
         LLNode *next;
         TrieNode *t;
     };
@@ -64,20 +64,47 @@ class Trie {
             if (len < key.size) {
                 int x = (key.data[len] > 90) ? key.data[len] - 97 + 26
                                              : key.data[len] - 65;
-                for (LLNode *b = (LLNode *)node->start; b != NULL; b = b->next)
-                    delete_recursive((TrieNode *)b);
-                if (curr->arr[x] == NULL) {
+                LLNode *aa = NULL;
+                for (LLNode *b = (LLNode *)curr->start; b != NULL;
+                     b = b->next) {
+                    if (b->letter == x) {
+                        aa = b;
+                        break;
+                    }
+                }
+                if (aa == NULL) {
                     TrieNode *new_node = (TrieNode *)malloc(sizeof(TrieNode));
                     new_node->letter = key.data[len];
                     new_node->children = 0;
-                    for (int i = 0; i < 52; i++) {
-                        new_node->arr[i] = NULL;
-                    }
                     new_node->value = NULL;
-                    curr->arr[x] = new_node;
+                    new_node->start = NULL;
+                    LLNode *l = (LLNode *)malloc(sizeof(LLNode));
+                    LLNode *b;
+                    if (curr->start == NULL ||
+                        ((LLNode *)curr->start)->letter > x) {
+                        if (curr->start != NULL)
+                            l->next = ((LLNode *)curr->start)->next;
+                        else
+                            l->next = NULL;
+                        curr->start = l;
+                        l->letter = x;
+                        l->t = new_node;
+                    } else {
+                        for (b = (LLNode *)curr->start;; b = b->next) {
+                            if (b->next == NULL || b->next->letter > x) {
+                                // l = b;
+                                break;
+                            }
+                        }
+                        l->next = b->next;
+                        b->next = l;
+                        l->letter = x;
+                        l->t = new_node;
+                    }
+                    aa = l;
                     // cout << &new_node << '\n';
                 }
-                curr = (TrieNode *)curr->arr[x];
+                curr = aa->t;
             } else if (len == key.size) {
                 // curr->value.data = value.data;
                 curr->value = (Slice *)malloc(sizeof(Slice));
@@ -101,9 +128,18 @@ class Trie {
             if (len < key.size) {
                 int x = (key.data[len] > 90) ? key.data[len] - 97 + 26
                                              : key.data[len] - 65;
-                if (curr->arr[x] == NULL)
+                LLNode *aa = NULL;
+                for (LLNode *b = (LLNode *)curr->start; b != NULL;
+                     b = b->next) {
+                    if (b->letter == x) {
+                        aa = b;
+                        break;
+                    }
+                }
+                if (aa == NULL) {
                     return 0;
-                curr = (TrieNode *)curr->arr[x];
+                }
+                curr = aa->t;
             } else if (len == key.size) {
                 if (curr->value == NULL)
                     return 0;
@@ -122,108 +158,108 @@ class Trie {
         // cout << endl;
     }
 
-    bool del(Slice &key) {
-        Slice v;
-        if (!get_val(key, v))
-            return 0;
-        int len = 0;
-        TrieNode *curr = root;
+    // bool del(Slice &key) {
+    //     Slice v;
+    //     if (!get_val(key, v))
+    //         return 0;
+    //     int len = 0;
+    //     TrieNode *curr = root;
 
-        while (curr != NULL) {
-            curr->children--;
-            if (len < key.size) {
-                int x = (key.data[len] > 90) ? key.data[len] - 97 + 26
-                                             : key.data[len] - 65;
-                if (curr->arr[x] == NULL)
-                    return 0;
-                TrieNode *p = curr;
-                curr = (TrieNode *)curr->arr[x];
-                if (p->children == 0 && p != root)
-                    free(p);
-                else {
-                    if (curr->children == 1)
-                        p->arr[x] = NULL;
-                }
-            } else if (len == key.size) {
-                if (curr->value == NULL)
-                    return 0;
-                else {
-                    // cout << curr->letter << " " << curr->children << endl;
-                    if (curr->children == 0) {
-                        free(curr);
-                    } else {
-                        free(curr->value);
-                        curr->value = NULL;
-                    }
-                    return 1;
-                }
-            }
-            len++;
-        }
+    //     while (curr != NULL) {
+    //         curr->children--;
+    //         if (len < key.size) {
+    //             int x = (key.data[len] > 90) ? key.data[len] - 97 + 26
+    //                                          : key.data[len] - 65;
+    //             if (curr->arr[x] == NULL)
+    //                 return 0;
+    //             TrieNode *p = curr;
+    //             curr = (TrieNode *)curr->arr[x];
+    //             if (p->children == 0 && p != root)
+    //                 free(p);
+    //             else {
+    //                 if (curr->children == 1)
+    //                     p->arr[x] = NULL;
+    //             }
+    //         } else if (len == key.size) {
+    //             if (curr->value == NULL)
+    //                 return 0;
+    //             else {
+    //                 // cout << curr->letter << " " << curr->children << endl;
+    //                 if (curr->children == 0) {
+    //                     free(curr);
+    //                 } else {
+    //                     free(curr->value);
+    //                     curr->value = NULL;
+    //                 }
+    //                 return 1;
+    //             }
+    //         }
+    //         len++;
+    //     }
 
-        return 1;
-    }
+    //     return 1;
+    // }
 
-    bool get_val_N(int n, Slice &key, Slice &value) {
-        string s;
-        int len = 0;
-        TrieNode *curr = root;
-        while (curr != NULL) {
-            // cout << curr->children << endl;
-            if (curr->children < n) {
-                return 0;
-            }
-            if (curr->children == 1) {
-                while (curr->value == NULL) {
-                    for (int j = 0; j < 52; j++) {
-                        if (curr->arr[j] != NULL) {
-                            curr = (TrieNode *)curr->arr[j];
-                            s += (j >= 26) ? j + 97 - 26 : j + 65;
-                            break;
-                        }
-                    }
-                }
-                key = s;
-                value.size = curr->value->size;
-                value.data = (char *)malloc(sizeof(char) * value.size);
-                for (int i = 0; i < value.size; i++) {
-                    value.data[i] = curr->value->data[i];
-                }
-                return 1;
-            }
-            if (n == 1 && curr->value != NULL) {
-                key = s;
-                value.size = curr->value->size;
-                value.data = (char *)malloc(sizeof(char) * value.size);
-                for (int i = 0; i < value.size; i++) {
-                    value.data[i] = curr->value->data[i];
-                }
-                return 1;
-            }
-            if (curr->value != NULL)
-                n--;
-            for (int j = 0; j < 52; j++) {
-                if (curr->arr[j] != NULL) {
-                    TrieNode *p = (TrieNode *)curr->arr[j];
-                    if (p->children >= n) {
-                        s += (j >= 26) ? j + 97 - 26 : j + 65;
-                        curr = p;
-                        break;
-                    } else {
-                        n -= p->children;
-                    }
-                }
-            }
-        }
-        // cur.arr['a'] = &cur;
-        // cout << endl;
-    }
+    // bool get_val_N(int n, Slice &key, Slice &value) {
+    //     string s;
+    //     int len = 0;
+    //     TrieNode *curr = root;
+    //     while (curr != NULL) {
+    //         // cout << curr->children << endl;
+    //         if (curr->children < n) {
+    //             return 0;
+    //         }
+    //         if (curr->children == 1) {
+    //             while (curr->value == NULL) {
+    //                 for (int j = 0; j < 52; j++) {
+    //                     if (curr->arr[j] != NULL) {
+    //                         curr = (TrieNode *)curr->arr[j];
+    //                         s += (j >= 26) ? j + 97 - 26 : j + 65;
+    //                         break;
+    //                     }
+    //                 }
+    //             }
+    //             key = s;
+    //             value.size = curr->value->size;
+    //             value.data = (char *)malloc(sizeof(char) * value.size);
+    //             for (int i = 0; i < value.size; i++) {
+    //                 value.data[i] = curr->value->data[i];
+    //             }
+    //             return 1;
+    //         }
+    //         if (n == 1 && curr->value != NULL) {
+    //             key = s;
+    //             value.size = curr->value->size;
+    //             value.data = (char *)malloc(sizeof(char) * value.size);
+    //             for (int i = 0; i < value.size; i++) {
+    //                 value.data[i] = curr->value->data[i];
+    //             }
+    //             return 1;
+    //         }
+    //         if (curr->value != NULL)
+    //             n--;
+    //         for (int j = 0; j < 52; j++) {
+    //             if (curr->arr[j] != NULL) {
+    //                 TrieNode *p = (TrieNode *)curr->arr[j];
+    //                 if (p->children >= n) {
+    //                     s += (j >= 26) ? j + 97 - 26 : j + 65;
+    //                     curr = p;
+    //                     break;
+    //                 } else {
+    //                     n -= p->children;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //     // cur.arr['a'] = &cur;
+    //     // cout << endl;
+    // }
 
-    bool del_N(int n) {
-        Slice a, b;
-        get_val_N(n, a, b);
-        del(a);
-    }
+    // bool del_N(int n) {
+    //     Slice a, b;
+    //     get_val_N(n, a, b);
+    //     del(a);
+    // }
 };
 
 int main(void) {
@@ -238,30 +274,34 @@ int main(void) {
 
     t.insert(a, val);
     t.insert(b, val);
-    t.insert(c, val);
+    // t.insert(c, val);
+    cout << t.get_val(b, val) << endl;
     t.insert(d, val);
+    cout << t.get_val(e, val) << endl;
     t.insert(e, val);
+    cout << t.get_val(e, val) << endl;
+    cout << t.get_val(a, val) << endl;
     // t.insert(c, val);
 
     // cout << t.del(a) << endl;
-    // cout << t.get_val(a, val) << endl;
+    cout << t.get_val(c, val) << endl;
     // t.del(b);
     // cout<< t.get_val(b, val) <<endl;
-    cout << t.del_N(1) << '\n';
-    cout << t.get_val_N(1, a, val) << '\n';
-    cout << a.data << '\n';
-    cout << val.data << '\n';
-    cout << t.get_val_N(2, a, val) << '\n';
-    cout << a.data << '\n';
-    cout << val.data << '\n';
-    cout << t.get_val_N(3, a, val) << '\n';
-    cout << a.data << '\n';
-    cout << val.data << '\n';
-    cout << t.get_val_N(4, a, val) << '\n';
-    cout << a.data << '\n';
-    cout << val.data << '\n';
-    cout << t.get_val_N(5, a, val) << '\n';
-    cout << a.data << '\n';
-    cout << val.data << '\n';
+    // cout << t.del_N(1) << '\n';
+    // cout << t.get_val_N(1, a, val) << '\n';
+    // cout << a.data << '\n';
+    // cout << val.data << '\n';
+    // cout << t.get_val_N(2, a, val) << '\n';
+    // cout << a.data << '\n';
+    // cout << val.data << '\n';
+    // cout << t.get_val_N(3, a, val) << '\n';
+    // cout << a.data << '\n';
+    // cout << val.data << '\n';
+    // cout << t.get_val_N(4, a, val) << '\n';
+    // cout << a.data << '\n';
+    // cout << val.data << '\n';
+    // cout << t.get_val_N(5, a, val) << '\n';
+    // cout << a.data << '\n';
+    // cout << val.data << '\n';
     return 0;
 }
