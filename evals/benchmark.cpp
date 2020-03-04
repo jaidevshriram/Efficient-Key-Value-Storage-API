a993d6e5a991a7c6193ddde5701030224824a40a
YoogottamK <yoogottamk@outlook.com>
2020-03-04 23:21:57 +0530

    added global locks

--- evals/benchmark.cpp
+++ evals/benchmark.cpp
@@ -50,7 +50,7 @@ string random_value(int stringLength) {
     return v;
 }
 
-const uint MAX_KEYS = 10000000, INSERTS = 100000, NUM_OPS = 0;
+const uint MAX_KEYS = 10000000, INSERTS = 100000, NUM_OPS = 1000000;
 const long CLOCKS_PER_SECOND = 1000000;
 const uint key_size = 64, val_size = 255;
 
--- evals/kvStore.cpp
+++ evals/kvStore.cpp
@@ -1,23 +1,34 @@
 #include <bits/stdc++.h>
 #include "trie_comp.cpp"
+#include <pthread.h>
+
 using namespace std;
 
 class kvStore {
     uint64_t maxSize;
+    pthread_rwlock_t lock;
+
 
    public:
     kvStore(uint64_t maxSize) {
         this->maxSize = maxSize;
+        pthread_rwlock_init(&lock, NULL);
     }
 
     Trie db;
 
     bool get(Slice &key, Slice &value) {
-        return db.get_val(key, value);
+        pthread_rwlock_rdlock(&lock);
+        bool ret = db.get_val(key, value);
+        pthread_rwlock_unlock(&lock);
+
+        return ret;
     }
 
     bool put(Slice &key, Slice &value) {
+        pthread_rwlock_wrlock(&lock);
         db.insert(key, value);
+        pthread_rwlock_unlock(&lock);
 
         /*
          * TODO
@@ -27,17 +38,28 @@ class kvStore {
     }
 
     bool del(Slice &key) {
-        return db.del(key);
+        pthread_rwlock_wrlock(&lock);
+        bool ret = db.del(key);
+        pthread_rwlock_unlock(&lock);
+
+        return ret;
     }
 
     bool get(int N, Slice &key, Slice &value) {
+        pthread_rwlock_rdlock(&lock);
         db.get_val_N(N, key, value);
+        pthread_rwlock_unlock(&lock);
+
         return true;
     }
 
     bool del(int N) {
         Slice key, value;
+
+        pthread_rwlock_wrlock(&lock);
         db.get_val_N(N, key, value);
+        pthread_rwlock_unlock(&lock);
+
         db.del(key);
         return true;
     }
